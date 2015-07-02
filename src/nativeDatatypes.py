4d8e9938800064eb53ec23a23d187547211f5315
rainyear <sniky.hack@gmail.com>
2015-07-02 17:11:25 +0800

    datatypes py :smiley:

--- src/nativeDatatypes.py
+++ src/nativeDatatypes.py
@@ -32,16 +32,22 @@ print("%s, %s, %s" % (s1, s2, s3))
 # Hello,
 # Dogge!
 
+## Length
+print(len(s1))      # 2
+
 ## Slicing
 s = '学而时习之'
 print('{0}:{1}'.format(s[0], s[-2]))    # 学:习
 
 # Byte
-# 0-255/x00-xff
+## 0-255/x00-xff
 byt = b'abc'
 print(type(byt))    # <class 'bytes'>
 print(byt[0] == 'a')# False
 print(byt[0] == 97) # True
+
+## Length
+print(len(byt))     # 3
 
 # Boolean
 True
@@ -51,3 +57,41 @@ print(type(True))   # <class 'bool'>
 # None
 print(None is None) # True
 print(type(None))   # <class 'NoneType'>
+
+# List
+l = ['python', 3, 'in', 'one']
+print(type(l))      # <class 'list'>
+
+## Length
+print(len(l))       # 4
+
+## Slicing
+print(l[0])         # 'python'
+print(l[-1])        # 'one'
+print(l[1:-1])      # [3, 'in']
+
+## Alter
+l.append('pic')     # None
+# l == ['python', 3, 'in', 'one', 'pic']
+l.insert(2, '.4.1') # None
+# l == ['python', 3, '.4.1', 'in', 'one', 'pic']
+l.extend(['!', '!'])
+# l == ['python', 3, '.4.1', 'in', 'one', 'pic', '!', '!']
+
+
+print(l.pop())             # '!'
+# l == ['python', 3, '.4.1', 'in', 'one', 'pic', '!']
+print(l.pop(2))           # '.4.1'
+# l == ['python', 3, 'in', 'one', 'pic', '!']
+l.remove("in")
+# l == ['python', 3, 'one', 'pic', '!']
+del l[2]
+# l == ['python', 3, 'pic', '!']
+
+print(l.index('pic'))       # 2
+
+# Tuple
+
+# Set
+
+# Dict
